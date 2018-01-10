97f3bc0d158cb8f0f381a0e6fe9ea744f23e05a0
Mazogu <michael_doom@hotmail.com>
2018-01-10 15:43:28 -0500

    Added views to java files

--- app/src/main/java/com/example/micha/viewsandlayouts/MainActivity.java
+++ app/src/main/java/com/example/micha/viewsandlayouts/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.micha.viewsandlayouts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView firstText;
+    private TextView secondText;
+    private Button doBtn;
+    private Button elseBthn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bindViews();
+    }
+
+    private void bindViews() {
+        firstText = findViewById(R.id.firstText);
+        secondText = findViewById(R.id.secondText);
+        doBtn = findViewById(R.id.btnSomething);
+        elseBthn = findViewById(R.id.btnElse);
     }
 }
