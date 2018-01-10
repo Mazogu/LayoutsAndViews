a3d710debd4b223e5c9178889d7f3f7d9d9f0cf0
Mazogu <michael_doom@hotmail.com>
2018-01-10 16:07:41 -0500

    Added listeners to the buttons

--- app/src/main/java/com/example/micha/viewsandlayouts/MainActivity.java
+++ app/src/main/java/com/example/micha/viewsandlayouts/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.micha.viewsandlayouts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView secondText;
     private Button doBtn;
     private Button elseBthn;
+    private EditText edit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +21,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         bindViews();
+
+        doBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String data = edit.getText().toString();
+                firstText.setText(data);
+            }
+        });
+
+        elseBthn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 
     private void bindViews() {
@@ -25,5 +43,6 @@ public class MainActivity extends AppCompatActivity {
         secondText = findViewById(R.id.secondText);
         doBtn = findViewById(R.id.btnSomething);
         elseBthn = findViewById(R.id.btnElse);
+        edit = findViewById(R.id.edit);
     }
 }
